9939b56ec71047f58fad562bb5448b325060e755
caffiendkitten <daneil.coulthard@gmail.com>
2020-02-27 11:06:46 -0800

    Updates

---
+++ precache-manifest.184722f3da641db7958169f38f27427c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ba06b17bda3dcf01a13f9bf2a7912b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bfc2e5d7865721ea090",
+    "url": "/static/css/main.980c0e71.chunk.css"
+  },
+  {
+    "revision": "0050f11d319fb0c33b6d",
+    "url": "/static/js/2.00579472.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.00579472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfc2e5d7865721ea090",
+    "url": "/static/js/main.eff6e5d5.chunk.js"
+  },
+  {
+    "revision": "824c1098d81b82d10eff",
+    "url": "/static/js/runtime-main.6a23a375.js"
+  }
+]);
