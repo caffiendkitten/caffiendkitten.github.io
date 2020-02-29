9f1689a276623172b595bc65fde9d8c631b7fab9
caffiendkitten <daneil.coulthard@gmail.com>
2020-02-28 17:47:12 -0800

    update to side bar navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,41 +17,86 @@ import Resources from '../containers/Resources.js'
 
 
 
-
-    // const Landing = () => (
-    //     <Home />
-    // )
-
 class Header extends Component {
 
     // componentDidMount = () => {
-    //     <Home  />
+    //     	var sidebarStatus = 'hidden';
     //   }
 
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            addClass: false,
+            navClass: "hide"
+        }
+    }
 
     handleOpenNav = () => {
-        document.getElementById("mySidenav").style.width = "250px";
-    }    
-    handleCloseNav = () => {
-        document.getElementById("mySidenav").style.width = "0";
-    }
+
+        if(this.state.addClass === false) {
+            this.setState({navClass: "active"});
+            this.setState({addClass: true});
+
+            document.getElementById("mySidenav").style.width = "250px";
+            // document.getElementById("hamburger").style.backgroundColor = "#ffffff";
+            document.getElementById('root').addEventListener('click',this.handleOpenNav )
+
+
+        } else {
+            this.setState({navClass: "hide"});
+            this.setState({addClass: false});
+
+            document.getElementById("mySidenav").style.width = "0";
+            // document.getElementByClassName("App").style.marginLeft = "0";
+            // document.getElementById("hamburger").style.backgroundColor = "#000000";
+            document.getElementById('root').removeEventListener('click',this.handleOpenNav )
+
+
+        }
+    }   
+
+    // handleOpenNav = () => {
+    //     document.getElementById("mySidenav").style.width = "250px";
+    //     document.getElementById("hamburger").style.backgroundColor = "#ffffff";
+    //     this.setState({addClass: !this.state.addClass});
+    // }   
+
+    // handleCloseNav = () => {
+    //     document.getElementById("mySidenav").style.width = "0";
+    //     document.getElementById("hamburger").style.backgroundColor = "#000000";
+    // this.setState({addClass: !this.state.addClass});
+    // }
 
 
 
     render(){
+        // let navClass = [""];
+        // if(this.state.addClass) {
+        //     navClass.push('active');
+        // }
         return <div className='navbar'>
-            <img src="header.jpeg" alt="image of books used for ethical hacking education"/>
-            <span className="navButton" onClick={this.handleOpenNav}>&#9776;</span>
+            <img src="header.jpeg" alt="books used for ethical hacking education"/>
+            {/* <span className="navButton" onClick={this.handleOpenNav}>&#9776;</span> */}
+            <div onClick={this.handleOpenNav} id="menu-icon">
+                <div className={this.state.navClass} id="hamburger"></div>
+            </div>
+
             {/* <span className="navButton" onClick={this.handleOpenNav}><i className="fa fa-list" aria-hidden="true"></i></span> */}
+            
+
             {/* <div className="glitch" data-text="Caffiendkitten">Caffiendkitten</div>  */}
             <h1>DaNeil Coulthard // CaffiendKitten</h1>
             <hr></hr>  
+                
             <Router>
   
             
             <div  id="mySidenav" className="sidenav">
-                <a href="#" className="closebtn"  onClick={this.handleCloseNav} >x</a>
+                {/* <a href="#" className="closebtn"  onClick={this.handleCloseNav} >x</a> */}
+                {/* <div className="active" onClick={this.handleCloseNav} id="menu-icon">
+                    <div className="active" id="hamburger"></div>
+                </div> 
+  */}
                 
                 <NavLink to="/" activeClassName="active">Home</NavLink>
                 <NavLink to="/blogs" activeClassName="chosen">Blogs</NavLink>
@@ -62,11 +107,11 @@ class Header extends Component {
             </div>
             <Switch>
                 <Route  path ="/" exact render={() => <Home  />} ></Route>
-                <Route path ="/blogs" exact render={props => <Blogs  />} ></Route>
-                <Route path ="/ctf" exact render={props => <CTF  />} ></Route>
-                <Route path ="/projects" exact render={props => <Projects  />} ></Route>
-                <Route path ="/ConsAndMeetups" exact render={props => <ConsAndMeetups  />} ></Route>
-                <Route path ="/resources" exact render={props => <Resources  />} ></Route>
+                <Route path ="/blogs" exact render={() => <Blogs  />} ></Route>
+                <Route path ="/ctf" exact render={() => <CTF  />} ></Route>
+                <Route path ="/projects" exact render={() => <Projects  />} ></Route>
+                <Route path ="/ConsAndMeetups" exact render={() => <ConsAndMeetups  />} ></Route>
+                <Route path ="/resources" exact render={() => <Resources  />} ></Route>
                 <Route component={Home}/>
             </Switch>
             </Router>
