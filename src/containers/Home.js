29f5756b00019cafaf9f277abe61f6e99f966052
caffiendkitten <daneil.coulthard@gmail.com>
2020-04-20 10:38:45 -0700

    spacing changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -77,21 +77,19 @@ const Home = () => {
                     Experienced in leveraging tool-based testing and manual hands-on testing of web 
                     applications and network services while utilizing web and network application 
                     guidelines and following industry-standard best practices and methodologies, such as OWASP and PTES.
-                </p>
+                </p><br />
                 <p>
                     I am passionate about Information Security and enthusiastic about finding 
                     security vulnerabilities. I am active within the Capture the Flag community 
                     and local tech meetup groups such as the CyberSecurity Non-Profit that I am a 
                     chapter organizer for, WISP, WoSec, and OWASP and this site will be built 
                     around my progress and my process of getting into Info Sec.
-
-                </p>
+                </p><br />
                 <p>
                     I am seeking a Vulnerability Researcher / Penetration Tester position that 
                     will push my experience and skills to the limit and also offer a challenging 
                     future with room for learning and advancement.
-                </p>
-
+                </p><br />
                 <p>
                 I started my transition into Information Security as a Web Dev Student at LCSC. 
                 From here I took an interest in Information Systems and went to City University of Seattle 
@@ -103,22 +101,18 @@ const Home = () => {
                 myself. During my time at Flatiron, I had the opportunity to work both collaboratively and 
                 independently to conceive and create multiple fullstack React and Ruby on Rails web applications 
                 in a fast-moving, high-stress environment.
+                </p><br />
 
+                <p>
+                    <b>Connect with me:</b><br />
+                    <b>LinkedIn:</b>  <a className="resourcesTable" href="https://www.linkedin.com/in/daneil-coulthard/"  target="_blank" rel="noopener noreferrer" >https://www.linkedin.com/in/daneil-coulthard/</a><br />
+                    <b>GitHub:</b> <a className="resourcesTable" href="https://github.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://github.com/caffiendkitten</a><br />
+                    <b>Dev.to:</b> <a className="resourcesTable" href="https://dev.to/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://dev.to/caffiendkitten</a><br />
+                    <b>PentesterLab:</b> <a className="resourcesTable" href="https://pentesterlab.com/profile/CaffiendKitten"  target="_blank" rel="noopener noreferrer" >https://pentesterlab.com/profile/CaffiendKitten</a><br />
+                    <b>HackThebox:</b> <a className="resourcesTable" href="https://www.hackthebox.eu/profile/67366"  target="_blank" rel="noopener noreferrer" >https://www.hackthebox.eu/profile/67366</a><br />
+                    <b>HackerOne:</b> <a className="resourcesTable" href="https://hackerone.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://hackerone.com/caffiendkitten</a><br />
+                    <b>BugCrowd:</b> <a className="resourcesTable" href="https://bugcrowd.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://bugcrowd.com/caffiendkitten</a><br />
                 </p>
-
-
-
-            <p><b>Connect with me:</b><br />
-            <b>LinkedIn:</b>  <a className="resourcesTable" href="https://www.linkedin.com/in/daneil-coulthard/"  target="_blank" rel="noopener noreferrer" >https://www.linkedin.com/in/daneil-coulthard/</a><br />
-            <b>GitHub:</b> <a className="resourcesTable" href="https://github.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://github.com/caffiendkitten</a><br />
-            <b>Dev.to:</b> <a className="resourcesTable" href="https://dev.to/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://dev.to/caffiendkitten</a><br />
-            <b>PentesterLab:</b> <a className="resourcesTable" href="https://pentesterlab.com/profile/CaffiendKitten"  target="_blank" rel="noopener noreferrer" >https://pentesterlab.com/profile/CaffiendKitten</a><br />
-            <b>HackThebox:</b> <a className="resourcesTable" href="https://www.hackthebox.eu/profile/67366"  target="_blank" rel="noopener noreferrer" >https://www.hackthebox.eu/profile/67366</a><br />
-            <b>HackerOne:</b> <a className="resourcesTable" href="https://hackerone.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://hackerone.com/caffiendkitten</a><br />
-            <b>BugCrowd:</b> <a className="resourcesTable" href="https://bugcrowd.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://bugcrowd.com/caffiendkitten</a><br />
-
-
-            </p>
 
 
             </div>
