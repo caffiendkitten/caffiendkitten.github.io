b786e6362f611785523ae64200a2ba2a786e9f33
caffiendkitten <daneil.coulthard@gmail.com>
2020-05-15 16:10:24 -0700

    added HTB CTF writeup

--- src/containers/CTF.js
+++ src/containers/CTF.js
@@ -52,6 +52,8 @@ const CTF = () => {
                         </p>
                         <p>
                         <b>Hack The Box </b> <br/>
+                            <a href="https://dev.to/caffiendkitten/htb-ctf-i-know-mag1k-2g7b" target="_blank" rel="noopener noreferrer" >I know Mag1k</a><br />                        
+                            <a href="https://dev.to/caffiendkitten/htb-ctf-freelancer-1ff6" target="_blank" rel="noopener noreferrer" >FreeLancer</a><br />                        
                             <a href="https://dev.to/caffiendkitten/htb-ctf-decode-me-3n8a" target="_blank" rel="noopener noreferrer" >Decode Me!!</a><br />                        
                             <a href="https://dev.to/caffiendkitten/htb-ctf-ezpz-2edg" target="_blank" rel="noopener noreferrer" >ezpz</a><br />                        
                         </p>
