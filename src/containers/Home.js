0421eadf86894e81816c33d18c9a52beca2261c2
caffiendkitten <daneil.coulthard@gmail.com>
2020-08-11 10:58:59 -0700

    added try hackme

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -32,13 +32,14 @@ const Home = () => {
                 </p>
                 <p>
                     <b>Connect with me:</b><br />
-                    <b>LinkedIn:</b>  <a className="resourcesTable" href="https://www.linkedin.com/in/daneil-coulthard/"  target="_blank" rel="noopener noreferrer" >https://www.linkedin.com/in/daneil-coulthard/</a><br />
-                    <b>GitHub:</b> <a className="resourcesTable" href="https://github.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://github.com/caffiendkitten</a><br />
-                    <b>Dev.to:</b> <a className="resourcesTable" href="https://dev.to/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://dev.to/caffiendkitten</a><br />
-                    <b>PentesterLab:</b> <a className="resourcesTable" href="https://pentesterlab.com/profile/CaffiendKitten"  target="_blank" rel="noopener noreferrer" >https://pentesterlab.com/profile/CaffiendKitten</a><br />
-                    <b>HackThebox:</b> <a className="resourcesTable" href="https://www.hackthebox.eu/profile/67366"  target="_blank" rel="noopener noreferrer" >https://www.hackthebox.eu/profile/67366</a><br />
-                    <b>HackerOne:</b> <a className="resourcesTable" href="https://hackerone.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://hackerone.com/caffiendkitten</a><br />
-                    <b>BugCrowd:</b> <a className="resourcesTable" href="https://bugcrowd.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >https://bugcrowd.com/caffiendkitten</a><br />
+                    <b>LinkedIn:</b>  <a className="resourcesTable" href="https://www.linkedin.com/in/daneil-coulthard/"  target="_blank" rel="noopener noreferrer" >linkedin.com/in/daneil-coulthard/</a><br />
+                    <b>GitHub:</b> <a className="resourcesTable" href="https://github.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >github.com/caffiendkitten</a><br />
+                    <b>Dev.to:</b> <a className="resourcesTable" href="https://dev.to/caffiendkitten"  target="_blank" rel="noopener noreferrer" >dev.to/caffiendkitten</a><br />
+                    <b>TryHackMe:</b> <a className="resourcesTable" href="https://tryhackme.com/p/CaffiendKitten"  target="_blank" rel="noopener noreferrer" >tryhackme.com/p/CaffiendKitten</a><br />
+                    <b>PentesterLab:</b> <a className="resourcesTable" href="https://pentesterlab.com/profile/CaffiendKitten"  target="_blank" rel="noopener noreferrer" >pentesterlab.com/profile/CaffiendKitten</a><br />
+                    <b>HackThebox:</b> <a className="resourcesTable" href="https://www.hackthebox.eu/profile/67366"  target="_blank" rel="noopener noreferrer" >hackthebox.eu/profile/67366</a><br />
+                    <b>HackerOne:</b> <a className="resourcesTable" href="https://hackerone.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >hackerone.com/caffiendkitten</a><br />
+                    <b>BugCrowd:</b> <a className="resourcesTable" href="https://bugcrowd.com/caffiendkitten"  target="_blank" rel="noopener noreferrer" >bugcrowd.com/caffiendkitten</a><br />
                 </p>
             </div>
             <div className="column right">
